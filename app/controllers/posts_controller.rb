316563d1e1723089f6dfe95e36c7ab1bb0134c72
tuannv1104 <tuannv1104@gmail.com>
2016-10-01 20:53:14 +0700

    Thêm chức năng xóa

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show,:edit,:update, :destroy]
 	def index
+		@posts = Post.all.order("created_at DESC")
 	end
 
 	def show
-		@post = Post.find(params[:id])
 	end
 
 	def new
@@ -20,12 +21,11 @@ class PostsController < ApplicationController
 	end
 
 
-	def edit
-		@post = Post.find(params[:id])
+	def edit	
 	end
 
 	def update
-		@post = Post.find(params[:id])
+		
 		if @post.update(post_params)
 			redirect_to @post
 		else
@@ -33,6 +33,15 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
+
+	def find_post
+		@post = Post.find(params[:id])	
+	end
+	
 	private
 	
 	def post_params
@@ -40,4 +49,5 @@ class PostsController < ApplicationController
 	end
 end
 
+	
 
