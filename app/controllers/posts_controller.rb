943ce3e40d9364bdf2239c009535ac331262a7df
tuannv1104 <tuannv1104@gmail.com>
2016-10-01 20:27:51 +0700

    create & edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,15 @@
 class PostsController < ApplicationController
 	def index
 	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
 	def new
 		@post = Post.new
 	end
+
 	def create
 		@post = Post.new(post_params)
 		if @post.save
@@ -12,10 +18,25 @@ class PostsController < ApplicationController
 			render "new"
 		end
 	end
+
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render "edit"
+		end
+	end
+
 	private
+	
 	def post_params
-		params.require(:post).permit(:title,:body)
-		
+		params.require(:post).permit(:title,:body)	
 	end
 end
 
