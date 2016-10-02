311da955b6279cbc10571918964de5299eddd993
tuannv1104 <tuannv1104@gmail.com>
2016-10-02 11:41:08 +0700

    tạo các ràng buộc của bài post

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161001163239) do
+ActiveRecord::Schema.define(version: 20161002035532) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
